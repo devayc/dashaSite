ed9be4e23735b0cecbd3f1fba571ff78f273f8eb
devayc <devayc3737@gmail.com>
2022-10-28 15:23:46 +0300

    ready gallary

--- js/main.js
+++ js/main.js
@@ -17,27 +17,27 @@ $('.owl-carousel').owlCarousel({
 })
 
 $(document).ready(function () {
-    $('[data-target]').click((function (event){
+    $('[data-target]').click((function (event) {
         event.preventDefault()
         const id = $(this).attr('href')
         console.log($(this))
         const top = $(id).offset().top;
         //анимируем переход на расстояние - top за 1500 мс
-        $('body,html').animate({scrollTop: top}, 1100);
+        $('body,html').animate({ scrollTop: top }, 1100);
     }))
 
     $("[data-modal]").click(event => {
-    console.log(event)
-    event.preventDefault()
-    $('.fixed-overlay__modal').fadeIn()
+        console.log(event)
+        event.preventDefault()
+        $('.fixed-overlay__modal').fadeIn()
     })
     $('.fixed-overlay').click((e) => {
-    const modalOverlay = document.querySelector('.fixed-overlay')
-    console.log(modalOverlay)
-    if (e.target === modalOverlay){
-        $('.fixed-overlay__modal').fadeOut()
-    }
-})
+        const modalOverlay = document.querySelector('.fixed-overlay')
+        console.log(modalOverlay)
+        if (e.target === modalOverlay) {
+            $('.fixed-overlay__modal').fadeOut()
+        }
+    })
     $('.close-modal').click(() => {
         $('.fixed-overlay__modal').fadeOut()
        
@@ -61,12 +61,36 @@ $(document).ready(function () {
         $('.bar').addClass('bar2')
     }
     function CloseMenu() {
-         $('.mob-menu').addClass('mob-menu-out')
-            $("#top").removeClass('top')
-            $("#bottom").removeClass('bottom')
-            $("#middle").removeClass('middle')
-            $('.bar').removeClass('bar2')
+        $('.mob-menu').addClass('mob-menu-out')
+        $("#top").removeClass('top')
+        $("#bottom").removeClass('bottom')
+        $("#middle").removeClass('middle')
+        $('.bar').removeClass('bar2')
     }
+    // $('#more').click(() => {
+    //     if ($('.gallary-items').find('.close-sm')) {
+    //         let small= $('.gallary-items').find('.close-sm')
+    //         $(small).removeClass('close-sm')
+    //         console.log('')
+    //     } else {
+    //         console.log('da')
+    //        let big = $('.gallary-items').find('.close-lg')
+    //             big.removeClass('close-lg')
+    //     }
+    // })
+    $('#more').click(() => {
+        const gallary = $('.gallary-item')
+        gallary.each((e) => {
+            console.log(gallary[e])
+            if ($(gallary[e]).hasClass('close-sm')) {
+                $(gallary[e]).removeClass('close-sm')
+               return false
+            } else if($(gallary[e]).hasClass('close-lg')){
+               $(gallary[e]).removeClass('close-lg')
+                $('#more').attr('disabled','disabled');
+            }
+        }) 
+    })
 });
 
 
