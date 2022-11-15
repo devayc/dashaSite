494d0cf697f997c9c21cb451cb02c896db85a11e
devayc <devayc3737@gmail.com>
2022-11-15 11:31:37 +0300

    add upbtn

--- js/main.js
+++ js/main.js
@@ -105,3 +105,22 @@ $(document).ready(function () {
   }
 })
 });
+
+$('body').append('<div class="upbtn"></div>');
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+        $('.upbtn').css({
+            transform: 'scale(1)'
+        });
+        } else {
+        $('.upbtn').css({
+            transform: 'scale(0)'
+        });
+    }
+});
+$('.upbtn').on('click',function() {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 500);
+    return false;
+});
